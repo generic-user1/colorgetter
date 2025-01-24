086f4eedfd3879e2e2e21c563f813f23e203b270
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2025-01-24 10:52:06 -0500

    Implement some potentially useful traits on ColoredWaterRun

--- src/colored_water.rs
+++ src/colored_water.rs
@@ -23,6 +23,52 @@ pub enum ColoredWaterUnit {
     Brown
 }
 
+impl From<ColoredWaterRun> for Vec<ColoredWaterUnit> {
+    fn from(value: ColoredWaterRun) -> Self {
+        let mut out = Vec::with_capacity(value.size);
+        for _ in 0..value.size {
+            out.push(value.color);
+        }
+        out
+    }
+}
+
+/// Reasons converting from a `&[ColoredWaterUnit]` may fail
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ColoredWaterRunError {
+    /// The source is empty and it is therefore impossible to know what color to use
+    Empty,
+
+    /// The given source has multiple colors
+    MismatchedColors
+}
+
+impl TryFrom<&[ColoredWaterUnit]> for ColoredWaterRun {
+    type Error = ColoredWaterRunError;
+    fn try_from(value: &[ColoredWaterUnit]) -> Result<Self, Self::Error> {
+        let mut encountered_color = None;
+        for &color_unit in value {
+            match encountered_color {
+                None => {
+                    encountered_color = Some(color_unit);
+                }
+                Some(encountered_color) if encountered_color != color_unit => {
+                    return Err(ColoredWaterRunError::MismatchedColors);
+                }
+                _ => ()
+            }
+        }
+        if let Some(color) = encountered_color {
+            Ok(ColoredWaterRun {
+                color,
+                size: value.len()
+            })
+        } else {
+            Err(ColoredWaterRunError::Empty)
+        }
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 /// A color represented in RGB format
 ///
