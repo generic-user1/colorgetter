6ea89edb7b963fee95bc893a5e3d3d78c5c10d6e
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2026-03-10 16:44:32 -0400

    Add a "success chance" estimate to gamestate scoring for demystification
    Success chance is calculated by substituting the top unknown color unit in a gamestate with each color it could possibly be, trying to find solutions for each resulting gamestate, and tallying up how many states were solvable
    
    Slows down demystification somewhat but improves reset rate and number of pours needed.
    Further improvements are possible by weighting individual successes by their likelihood of occurring

--- src/solution/demystify.rs
+++ src/solution/demystify.rs
@@ -1,7 +1,13 @@
 //! utilities for demystification of [PartialGameState]s into [KnownGameState](crate::gamestate::KnownGameState)s
 
+use std::collections::{hash_map::Entry, HashMap};
+
 use super::*;
-use crate::gamestate::{GameState, PartialGameState};
+use crate::{
+    bottle::Bottle,
+    colored_water::{ColoredWaterIter, ColoredWaterUnit, PartialColoredWaterUnit},
+    gamestate::{GameState, PartialGameState}
+};
 
 /// Statistics regarding confidence in [try_demystify_next_step]'s result
 #[derive(Default)]
@@ -44,7 +50,13 @@ pub fn try_demystify_next_step<'a, const MAX_BCOUNT: usize, const B_MAX_CAP: usi
         for pour in possible_solution.get_pours() {
             working_gs = pour.try_apply(&working_gs).unwrap()
         }
-        let score = working_gs.pours_to_finish_estimate();
+        let base_score = working_gs.pours_to_finish_estimate();
+
+        //increase the score by some amount proportional to how likely this state is to be a dead end
+        //the penalty multiplier is 1.0 if the success chance is 1.0, and 2.0 if the success chance is 0.0
+        let failure_chance_penalty_mult = (1.0 - rate_success_chance(&working_gs)) + 1.0;
+        let score = ((base_score as f64) * failure_chance_penalty_mult) as usize;
+
         if score < min_pours_estimate {
             min_pours_estimate = score;
             min_scoring_solution = Some(possible_solution);
@@ -64,4 +76,77 @@ pub fn try_demystify_next_step<'a, const MAX_BCOUNT: usize, const B_MAX_CAP: usi
             }
         )
     })
+}
+
+/// Rate how likely it is for some partial gamestate to be possible to progress from by replacing the top unknown unit with each color,
+/// trying to find a solution, and counting how many possibilities resulted in a solvable state.
+///
+/// A return value of 1.0 indicates a 100% chance of progression from the given state, and a return value of 0.0 returns a 0% chance of progression
+/// (i.e. a 100% chance for a dead-end)
+/// Returns 1.0 for gamestates with no top unit that is unknown.
+///
+/// Currently, does not consider how likely it is for the unknown unit to be any particular color; will skip colors that aren't
+/// possible (because too many of that color are already known) but that's it. This aspect may be improved in the future.
+///
+/// If, somehow, there are multiple bottles whose topmost units are unknown, only tests with the first occurring unit
+fn rate_success_chance<const MAX_BCOUNT: usize, const B_MAX_CAP: usize>(
+    gamestate_to_rate: &PartialGameState<MAX_BCOUNT, B_MAX_CAP>
+) -> f64 {
+    //first, get color counts for known units and max capacity of the largest bottle;
+    //these will be used to ensure we don't add a unit of a color that would lead to too many units of that color
+    let mut color_counts: HashMap<ColoredWaterUnit, usize> = HashMap::new();
+    let mut largest_cap_seen = 0;
+    for bottle in gamestate_to_rate.bottles.iter() {
+        if bottle.capacity() > largest_cap_seen {
+            largest_cap_seen = bottle.capacity();
+        }
+        if let Some(top_idx) = bottle.get_top_content_idx() {
+            for idx in 0..=top_idx {
+                if let Some(color) = bottle.sample_known_color_at(idx) {
+                    match color_counts.entry(color) {
+                        Entry::Occupied(mut e) => {
+                            *e.get_mut() = e.get() + 1;
+                        }
+                        Entry::Vacant(e) => {
+                            e.insert(1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //next, find the bottle with an unknown unit on top
+    for (bottle_idx, bottle) in gamestate_to_rate.bottles.iter().enumerate() {
+        if bottle.get_top_color() == Some(PartialColoredWaterUnit::UnknownColor) {
+            //once we have our bottle, start trying colors
+            let top_idx = bottle.get_top_content_idx().unwrap();
+
+            let mut trial_count = 0_usize;
+            let mut success_count = 0_usize;
+            for color in ColoredWaterIter(None) {
+                //determine if adding one unit of this color would lead to too many units of that color
+                let this_color_current_count = *color_counts.get(&color).unwrap_or(&0);
+                if this_color_current_count + 1 > largest_cap_seen {
+                    //if it would, skip checking this color
+                    continue;
+                }
+
+                let mut sim_state = gamestate_to_rate.clone();
+                sim_state.bottles[bottle_idx]
+                    .try_set_color(top_idx, Some(PartialColoredWaterUnit::Color(color)))
+                    .unwrap();
+                trial_count += 1;
+
+                if Solution::try_new(&sim_state, 0).is_some() {
+                    success_count += 1;
+                }
+            }
+
+            let success_chance = (success_count as f64) / (trial_count as f64);
+            return success_chance;
+        }
+    }
+    //we reach here if there was no bottle with an unknown unit on top, return 1.0 by default
+    1.0
 }
